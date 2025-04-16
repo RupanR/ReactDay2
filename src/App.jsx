af6e58ec3033fb9fcfcf1546d49ed30f55666f94
RupanR <rupanmentor@gmail.com>
2025-04-16 20:09:56 +0530

    component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-//import part
 import React from 'react';
+import CompA from "./Components/CompA/CompA"
+import CompB from './Components/CompB/CompB';
+import CompC from './Components/CompC/CompC';
+import Home from './Components/Home/Home';
 
 const App = () => {
-    //js part
     return (
-        //html part
-        <>
-            <h1>Hello World</h1>
-        </>
+        <div>
+            <h1>App Component</h1>
+           <CompA />
+           <CompB />
+           <CompC />
+           <Home />
+        </div>
     );
 };
 
-//export part
 export default App;
+
