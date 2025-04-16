8e16a6faab53ced4e92b3064a1f6efd804cb5307
RupanR <rupanmentor@gmail.com>
2025-04-16 20:52:33 +0530

    js part completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Home = () => {
+
+ let name = "FSD-WD-T-B21";  
+  const handleSubmit = () =>{
+    console.log("Button Clicked");
+  }
+
+
   return (
     <div>
       <h1 id="home">Home Component</h1>
+      <button onClick={handleSubmit}>Click Me!</button>
+      <br />
+      <br />
+      <br />
+     {name}
     </div>
   );
 };
