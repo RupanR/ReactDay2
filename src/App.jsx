958bd03be57707c0d3ccd6d54e3957f9e438a613
RupanR <rupanmentor@gmail.com>
2025-04-16 19:22:00 +0530

    base code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import part
+import React from 'react';
+
+const App = () => {
+    //js part
+    return (
+        //html part
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+};
+
+//export part
+export default App;
